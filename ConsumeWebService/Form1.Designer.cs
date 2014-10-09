b69a4ece978d21613c09c5213355cd94d2802f36
EricLoos <eric.loos@gmail.com>
2014-10-08 20:34:55 -0700

    First Git check in

---
+++ ConsumeWebService/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ConsumeWebService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsnOnTimer = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(549, 321);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Description";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "#";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 30;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Value";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.tsnOnTimer,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 114);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // tsnOnTimer
+            // 
+            this.tsnOnTimer.Checked = true;
+            this.tsnOnTimer.CheckOnClick = true;
+            this.tsnOnTimer.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsnOnTimer.Name = "tsnOnTimer";
+            this.tsnOnTimer.Size = new System.Drawing.Size(124, 22);
+            this.tsnOnTimer.Text = "On Timer";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Checked = true;
+            this.toolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(124, 22);
+            this.toolStripMenuItem2.Text = "1";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(124, 22);
+            this.toolStripMenuItem3.Text = "2";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(124, 22);
+            this.toolStripMenuItem4.Text = "4";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 364);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "CaseFax Alerts";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolStripMenuItem tsnOnTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ ConsumeWebService/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsumeWebService
+{
+    public partial class Form1 : Form
+    {
+        string[] titles = { "CaseFax Pending Count", "Court Pending Count", 
+                              "Maximum Pending Days", "Max Pending Court Days",
+                          "Count over 2 court days",
+                          "Support Tickets","Urgent Support Tickets",
+                          "Alerts",
+                          "Reversals Pending", "Reversals Approved",
+                          "Caller ID Issues",
+                          "Time pending out of balance"
+                          };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ServiceReference1.GetAlertsSoapClient sr = new ServiceReference1.GetAlertsSoapClient();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LastBalanced = DateTime.Now;
+            GetAlerts();
+        }
+        public DateTime LastBalanced = DateTime.Now;
+        public void GetAlerts()
+        {
+            Cursor = Cursors.WaitCursor;
+            string s = sr.GetCounts("CaseFaxAlertsPa55word77!");
+            s += ",00:00";
+            string[] items = s.Split(',');
+            if (items[0] == items[1])
+            {
+                LastBalanced = DateTime.Now;
+            }
+            if (listView1.Items.Count > 0)
+            {
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    ListViewItem li = listView1.Items[i];
+                    li.SubItems[2].Text = items[i];
+                    switch (i + 1)
+                    {
+                        case 6:
+
+                            if (items[i] == "0")
+                                li.BackColor = Color.White;
+                            else
+                            {
+                                li.BackColor = Color.Yellow;
+                                //li.ForeColor = Color.White;
+                            }
+                            break;
+                        case 7:
+                            if (items[i] == "0")
+                            {
+                                li.BackColor = Color.White;
+                                li.ForeColor = Color.Black;
+                            }
+                            else
+                            {
+                                li.BackColor = Color.Red;
+                                li.ForeColor = Color.White;
+                            }
+                            break;
+                        case 10:
+                        case 11:
+                            if (items[i] == "0")
+                            {
+                                li.BackColor = Color.White;
+                                li.ForeColor = Color.Black;
+                            }
+                            else
+                            {
+                                li.BackColor = Color.Red;
+                                li.ForeColor = Color.White;
+                            }
+                            break;
+                        case 12:
+                            li.BackColor = Color.White;
+                            li.ForeColor = Color.Black;
+                            /*
+                            string[] v = li.SubItems[2].Text.Split(':');
+                            int j;
+                            if (int.TryParse(v[1], out j))
+                            {
+                                if (j > 10)
+                                {
+                                    li.BackColor = Color.Yellow;
+                                }
+                                if (j > 20)
+                                {
+                                    li.BackColor = Color.Red;
+                                    li.ForeColor = Color.White;
+                                }
+                            } */
+                            if (MinutesOut > 10)
+                                li.BackColor = Color.Yellow;
+                            if (MinutesOut > 20)
+                            {
+                                li.BackColor = Color.Red;
+                                li.ForeColor = Color.White;
+                            }
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                int i = 0;
+                if (items.Length == titles.Length)
+                {
+                    foreach (string item in items)
+                    {
+                        ListViewItem li = new ListViewItem();
+                        li.Text = titles[i]; //string.Format("{0}", i + 1); ;
+                        li.SubItems.Add(string.Format("{0}", i + 1));//titles[i]);
+                        li.SubItems.Add(item);
+                        listView1.Items.Add(li);
+                        //listView1.Items.Add(titles[i]);
+
+                        i++;
+                    }
+                }
+            }
+            TimeSpan ts = DateTime.Now - LastBalanced;
+            MinutesOut = ts.TotalMinutes;
+            if (ts.TotalSeconds > 0)
+            {
+                int secs = (int)ts.TotalSeconds;
+                ListViewItem li = listView1.Items[listView1.Items.Count - 1];
+                li.SubItems[2].Text = string.Format("{0:00}:{1:00}", secs / 60, secs % 60);
+            }
+            Cursor = Cursors.Default;
+            AlertCount++;
+        }
+        double MinutesOut = 0.0;
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetAlerts();
+        }
+        public int seconds = 0;
+        public int minutes = 0;
+        public int AlertCount = 0;
+        public int MaxAlertMinutes = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tsnOnTimer.Checked)
+            {
+                seconds++;
+                if (seconds >= 60)
+                {
+                    seconds = 0;
+                    minutes++;
+                    if (minutes >= MaxAlertMinutes)
+                    {
+                        minutes = 0; 
+                        GetAlerts();
+                        //AlertCount++;
+                    }
+                }
+            }
+            label1.Text = string.Format("{0} -- {1:00}:{2:00}", AlertCount, minutes, seconds);
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            if (sender is ToolStripMenuItem)
+            {
+                string s = (sender as ToolStripMenuItem).Text;
+                if (int.TryParse(s, out i))
+                {
+                    MaxAlertMinutes = i;
+                    ClearChecks();
+                    (sender as ToolStripMenuItem).Checked = true;
+                }
+            }
+        }
+        private void ClearChecks()
+        {
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+        }
+    }
+}
